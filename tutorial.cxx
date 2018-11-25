fc3b83ff99614dd9f59326b39b9bf36bce493d87
Li Jian Wu <ljwshr@126.com>
2018-11-25 10:13:34 +0800

    chore: build a library and executable

--- tutorial.cxx
+++ tutorial.cxx
@@ -1,18 +1,29 @@
 // A simple program that computes the square root of a number
+
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+// include our own function
+#  include "MathFunctions.h"
+
+
 int main(int argc, char* argv[])
 {
   if (argc < 2) {
-    fprintf(stdout, "%s Version test\n", argv[0]
-            );
+    fprintf(stdout, "%s Version test\n", argv[0]);
     fprintf(stdout, "Usage: %s number\n", argv[0]);
     return 1;
   }
+
   double inputValue = atof(argv[1]);
-  double outputValue = sqrt(inputValue);
+  double outputValue = 0;
+
+  if (inputValue >= 0) {
+	// use our own function
+    outputValue = mysqrt(inputValue);
+  }
+
   fprintf(stdout, "The square root of %g is %g\n", inputValue, outputValue);
   return 0;
 }
